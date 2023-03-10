32f195b53581a15545e249e19ad914d7f5c096cd
s722143 <s722143@gmail.com>
2023-03-10 08:39:28 +0530

    initial project commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          An Amazing Startup idea !!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Assignment Done. Lear Code Online woo!!
+        </p>
+      
       </header>
     </div>
   );
